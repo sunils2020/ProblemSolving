158e97a9daafcdc721d43771a2f423f5e6dbb47a
sunils2020 <sunilk.gunasekaran@gmail.com>
2021-10-29 00:10:44 +0530

    Factor based

--- src/main/java/PrimeNumbers/NumberOfOpenDoors.java
+++ src/main/java/PrimeNumbers/NumberOfOpenDoors.java
@@ -21,14 +21,13 @@ public class NumberOfOpenDoors {
     //  Consider number - 9
     //  1   3   9   => Odd number of factors. This means the door would be finally OPEN
     //
+    //  Solution 1 : Take each number and find the corresponding factors of it - if the factor is odd then count that particular number
+    //  In our case, it would be 1, 4 => Results in 2.. This is O(n) solution
     //
+    //  Better Solution:    To find the number of factors - it is enough to find the Square root of the number to find its factors
+    //  Example:   Math.sqrt(10) = 3    ==> Numbers less than 3 which are divisible by 3 are 1, 2
+    //  Now we know the total number of doors = 5, maximum doors which can be open should be square root of (5)
     //
-    //
-    //
-    //
-
-
-
     public int solve(int doors) {
         int count = 0;
         for(int i=0;i<(int)Math.sqrt(doors);i++){
