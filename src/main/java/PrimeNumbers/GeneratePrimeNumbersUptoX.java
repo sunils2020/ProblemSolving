524229f0a24417056a9f995ff7d6947c3249baaf
sunils2020 <sunilk.gunasekaran@gmail.com>
2021-10-30 01:00:30 +0530

    Factor based

---
+++ src/main/java/PrimeNumbers/GeneratePrimeNumbersUptoX.java
@@ -0,0 +1,39 @@
+package PrimeNumbers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class GeneratePrimeNumbersUptoX {
+    public static void main(String[] args) {
+        GeneratePrimeNumbersUptoX obj = new GeneratePrimeNumbersUptoX();
+        obj.getAllPrimeNumbers(100);
+    }
+
+    //  Generate all prime numbers
+    //                                      2   3   4   5   6   7   8   9   10
+    //  => Set all values to 0  =>          1   1   1   1   1   1   1   1   1
+    //  => From 2 - multiples of 2 mark 0   1   1   0   1   0   1   0   1   0
+    //  Multiples of 3                      1   1   0   1   0   1   0   0   0
+    //   4                                  1   1   0   5   0   1   0   0   0
+    //   5
+    //  Continue to do till end of the array
+    //  Array value which retains 1 are the list of prime numbers at the end
+    //
+
+    public ArrayList<Integer> getAllPrimeNumbers(int number){
+        ArrayList<Integer> result = new ArrayList<>();
+        int[] arrays = new int[number];
+        for (int i=2;i<number;i++){
+            arrays[i] = 1;
+        }
+        for (int i=2;i<number;i++) {
+            int dividingFactor = i;
+            if (arrays[dividingFactor]==0) continue;
+            for (int j = i * 2; j < arrays.length; j = j + i) {
+                arrays[j] = 0;
+            }
+            result.add(dividingFactor);
+        }
+        return result;
+    }
+}
